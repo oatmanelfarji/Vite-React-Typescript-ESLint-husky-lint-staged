cdac80bdcfa2ac98d753eaa4aa06b6a98fa79c1c
oatman <elfarjiotheman@gmail.com>
2025-06-19 20:53:49 +0100

    Vite + React + Typescript + ESLint + husky + lint-staged

---
+++ src/app.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <h1>Vite + React + Typescript + ESLint + husky + lint-staged + Github Actions</h1>
+      </div>
+      <div className="card">
+        <button onClick={() => setCount(count => count + 1)} type="button">
+          count is
+          {" "}
+          {count}
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
